be0a1e4b4c91a5405907fce5923e1714fc16ff85
Leonardo Alves <leonardo.alves779@gmail.com>
2023-04-12 18:35:58 -0300

    adding brand section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Feature } from '../../components/feature/Feature'
 import './whatGPT3.css'
 
 export const WhatGPT3 = () => {
   return(
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+    </div>
   )
 }
